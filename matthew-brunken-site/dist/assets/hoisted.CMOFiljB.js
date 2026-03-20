adbae8a3917e38299f0bf65db1295d4e1d782e66
Matthew Brunken <m.alex.brunken@gmail.com>
2026-03-20 09:20:28 -0500

    Delete matthew-brunken-site/dist directory

--- matthew-brunken-site/dist/assets/hoisted.CMOFiljB.js
+++
@@ -1,1 +0,0 @@
-const e=document.querySelector(".mobile-menu-btn"),t=document.querySelector(".nav-links");e&&t&&e.addEventListener("click",()=>{t.classList.toggle("active");const n=t.classList.contains("active");e.setAttribute("aria-expanded",n.toString())});
